c8a06b506dbf13a783934dd6be0e8508c54e2b00
NotHolyFire <163702132+NotHolyFire@users.noreply.github.com>
2025-02-25 15:30:29 -0300

    Add files via upload

---
+++ Content/Pets/Juberto/JubertoBuff.cs
@@ -0,0 +1,20 @@
+using Terraria.ModLoader;
+using Terraria;
+
+namespace TheDeep.Content.Pets.Juberto
+{
+    public class JubertoBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        { // This method gets called every frame your buff is active on your player.
+            bool unused = false;
+            player.BuffHandle_SpawnPetIfNeededAndSetTime(buffIndex, ref unused, ModContent.ProjectileType<Juberto>());
+        }
+    }
+}
