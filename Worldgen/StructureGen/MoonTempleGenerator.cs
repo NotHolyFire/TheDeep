70cc061bebd8380766c7ffb742194c073af589d5
NotHolyFire <163702132+NotHolyFire@users.noreply.github.com>
2026-07-30 00:27:37 -0300

    Add files via upload

---
+++ Worldgen/StructureGen/MoonTempleGenerator.cs
@@ -0,0 +1,23 @@
+using StructureHelper;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace TheDeep.Content.Worldgen.StructureGen
+{
+    public class MoonTempleGenerator : ModSystem
+    {
+        
+
+       
+        
+    }
+
+    public class GenPass
+    {
+
+    }
+}
