1054a5f06f15436405eb0b810a20eab1b315cee9
NotHolyFire <163702132+NotHolyFire@users.noreply.github.com>
2026-07-30 01:11:59 -0300

    Add files via upload

---
+++ Content/Items/Consumables/Bait/Special/KelptopusBait.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheDeep.Content.Items.Fish;
+
+namespace TheDeep.Content.Items.Consumables.Bait.Special
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class KelptopusBait : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 30;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true;
+            Item.bait = 5;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.buyPrice(copper: 0);
+        }
+    }
+}
