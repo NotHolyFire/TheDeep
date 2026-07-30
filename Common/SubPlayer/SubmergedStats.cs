8b7d8c79831e0dd0ecd6b00cc5ba30d1a973982d
NotHolyFire <163702132+NotHolyFire@users.noreply.github.com>
2026-07-30 13:29:27 -0300

    Update SubmergedStats.cs

--- Common/SubPlayer/SubmergedStats.cs
+++ Common/SubPlayer/SubmergedStats.cs
@@ -8,10 +8,12 @@ namespace TheDeep.Common.SubPlayer
         Player player => Main.LocalPlayer;
         public int Hostility;
         public float TreasureChance;
+        public bool CreatureCatch;
         public override void ResetEffects()
         {
             Hostility = 0; // Increases chance of hooking enemies
             TreasureChance = 1f; // Starts at 1f (0) Increases or decreases chance of hooking crates/treasures
+            CreatureCatch = false;
         }
 
         public override void ModifyFishingAttempt(ref FishingAttempt attempt)
@@ -25,6 +27,12 @@ namespace TheDeep.Common.SubPlayer
             {
                 attempt.crate = false;
             }
+
+            if (Main.rand.Next(100) < Hostility)
+            {
+                CreatureCatch = true;
+            }
+
         }
     }
 }
