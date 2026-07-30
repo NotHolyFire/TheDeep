2a934648090d0b2e0377d2c3ce74956a5301b66a
NotHolyFire <163702132+NotHolyFire@users.noreply.github.com>
2026-07-30 00:24:07 -0300

    Add files via upload

---
+++ Content/Dusts/ChargeShotDust.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TheDeep.Content.Dusts
+{
+    public class ChargeShotDust : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true; 
+            dust.scale *= 1.5f;
+        }
+
+        public override bool Update(Dust dust)
+        { 
+            dust.position += dust.velocity;
+            dust.rotation += dust.velocity.X * 0.15f;
+            dust.scale *= 0.99f;
+
+            float light = 0.35f * dust.scale;
+
+            Lighting.AddLight(dust.position, light, light, light);
+
+            if (dust.scale < 0.5f)
+            {
+                dust.active = false;
+            }
+
+            return false;
+        }
+    }
+}
